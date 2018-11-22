923356948a3057f4b0bd51abc6eb14e13c914d40
Jakub_Patzer_Scalac <jakub_patzer_scalac@MacBook-Pro-Jakub-Patzer-Scalac.local>
2018-11-22 12:47:36 +0100

    Updates

---
+++ precache-manifest.7a464e6ff05d332c93a9b08fd88455e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16ae1fbee4f5b78fe6f7",
+    "url": "/react-todo-list/static/css/main.b8e30eb7.chunk.css"
+  },
+  {
+    "revision": "16ae1fbee4f5b78fe6f7",
+    "url": "/react-todo-list/static/js/main.16ae1fbe.chunk.js"
+  },
+  {
+    "revision": "f961a679595564d6fe87",
+    "url": "/react-todo-list/static/css/1.118f6f6a.chunk.css"
+  },
+  {
+    "revision": "f961a679595564d6fe87",
+    "url": "/react-todo-list/static/js/1.f961a679.chunk.js"
+  },
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "76b5b5aaefdb4d965143c262500b25d5",
+    "url": "/react-todo-list/index.html"
+  }
+];
